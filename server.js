34a7907f1762b9b474c1adc51586b36194037430
Juampi8524 <elmar8511paris@gmail.com>
2026-04-05 10:38:52 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
-Const express = require(‘express’);
-Const http = require(‘http’);
-Const { Server } = require(‘socket.io’);
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 
-Const app = express();
-Const server = http.createServer(app);
-Const io = new Server(server);
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-// Le decimos al servidor que muestre tu HTML que está en la carpeta “public”
-App.use(express.static(‘public’));
+// Le decimos al servidor que muestre tu HTML que está en la carpeta "public"
+app.use(express.static('public'));
 
 // Aquí detectamos cuando un jugador abre el juego
-Io.on(‘connection’, (socket) => {
-    Console.log(‘Un jugador se ha conectado. ID:’, socket.id);
+io.on('connection', (socket) => {
+    console.log('Un jugador se ha conectado. ID:', socket.id);
 
     // Cuando el jugador cierra la pestaña
-    Socket.on(‘disconnect’, () => {
-        Console.log(‘El jugador se ha desconectado. ID:’, socket.id);
+    socket.on('disconnect', () => {
+        console.log('El jugador se ha desconectado. ID:', socket.id);
     });
 });
 
 // Render nos da un puerto automático, si no, usamos el 3000
-Const PORT = process.env.PORT || 3000;
-Server.listen(PORT, () => {
-    Console.log(`Servidor de FutCard corriendo en el puerto ${PORT}`);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Servidor de FutCard corriendo en el puerto ${PORT}`);
 });
-
